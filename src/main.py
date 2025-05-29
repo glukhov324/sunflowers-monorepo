d271143d70261c2d16d4c38160593376586b93bc
glukhov324 <glukhovv.nn@gmail.com>
2025-05-29 09:32:24 +0700

    update cors

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api import router
-from src.settings import settings
 
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
